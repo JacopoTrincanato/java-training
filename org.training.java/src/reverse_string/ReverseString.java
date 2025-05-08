0833e0c1a31dde903065328b67b41d58d7e00496
JacopoTrincanato <trincanatoj@gmail.com>
2025-05-08 11:23:12 +0200

    create ReverseString.java class in reverse_string folder

---
+++ org.training.java/src/reverse_string/ReverseString.java
@@ -0,0 +1,17 @@
+package reverse_string;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        // use string concatenation operator(+)
+        String string = "abcd";
+        String reverseString = "";
+
+        int stringLength = string.length();
+
+        for (int i = stringLength - 1; i >= 0; i--) {
+            reverseString = reverseString + string.charAt(i);
+        }
+
+        System.out.println(reverseString);
+    }
+}
