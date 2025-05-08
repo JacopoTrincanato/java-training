3de142bd0ee6ad9072819ce5134e84efa24e56fa
JacopoTrincanato <trincanatoj@gmail.com>
2025-05-08 10:15:14 +0200

    add logic for swap number in another two ways

--- org.training.java/src/swap_numbers/SwapNumbers.java
+++ org.training.java/src/swap_numbers/SwapNumbers.java
@@ -13,5 +13,16 @@ public class SwapNumbers {
         numberTwo = numberThree;
 
         System.out.println("After swapping values are... " + numberOne + " " + numberTwo);
+
+        // swap values in a different ways using addictions and subtractions
+        numberOne = numberOne + numberTwo; // 10+20=30
+        numberTwo = numberOne - numberTwo; // 30-20=10
+        numberOne = numberOne - numberTwo; // 30-10=20
+
+        // swap numbers using moltiplications and divisions. Here numberOne and
+        // numberTwo values cannot be zero
+        numberOne = numberOne * numberTwo; // 10*20=200
+        numberTwo = numberOne / numberTwo; // 200/20=10
+        numberOne = numberOne / numberTwo; // 200/10=20
     }
 }
