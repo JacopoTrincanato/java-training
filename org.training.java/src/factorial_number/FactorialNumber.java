096abc232a343c5e37f1ef4ed961093511228e89
JacopoTrincanato <trincanatoj@gmail.com>
2025-05-13 22:36:55 +0200

    implement a second way to find the factorial of a number

--- org.training.java/src/factorial_number/FactorialNumber.java
+++ org.training.java/src/factorial_number/FactorialNumber.java
@@ -4,12 +4,20 @@ public class FactorialNumber {
     public static void main(String[] args) {
 
         int num = 15;
+        int numTwo = 10;
         long factorial = 1;
+        long factorialTwo = 1;
 
         for (int i = 1; i <= num; i++) {
             factorial = factorial * i;
         }
 
+        // alternative way
+        for (int i = numTwo; i >= 1; i--) {
+            factorialTwo = factorialTwo * i;
+        }
+
         System.out.println(factorial);
+        System.out.println(factorialTwo);
     }
 }
