fc7cf993f34285bcfaf8ebef13f78dfc3b44e84f
JacopoTrincanato <trincanatoj@gmail.com>
2025-05-13 18:08:56 +0200

    add a way to count also the number of even digits and odd digits

--- org.training.java/src/numb_of_digits/NumbOfDigits.java
+++ org.training.java/src/numb_of_digits/NumbOfDigits.java
@@ -5,11 +5,24 @@ public class NumbOfDigits {
         int num = 123456;
         int count = 0;
 
+        int evenCount = 0;
+        int oddCount = 0;
+
         while (num > 0) {
             num = num / 10;
             count++;
+
+            if (num % 2 == 1) {
+                evenCount++;
+            }
+
+            if (num % 2 == 0) {
+                oddCount++;
+            }
         }
 
         System.out.println("Number of digits: " + count);
+        System.out.println("Number of even digits: " + evenCount);
+        System.out.println("Number of odd digits: " + oddCount);
     }
 }
