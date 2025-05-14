e9219a8b5a70f0e0fa96cb30459a640039bb1805
JacopoTrincanato <trincanatoj@gmail.com>
2025-05-14 14:54:13 +0200

    implement a different way for check if two arrays are equals

--- org.training.java/src/check_array_equal/EqualArrays.java
+++ org.training.java/src/check_array_equal/EqualArrays.java
@@ -5,12 +5,33 @@ import java.util.Arrays;
 public class EqualArrays {
     public static void main(String[] args) {
 
+        // approach 1
         int[] arrOne = { 1, 2, 3, 4, 5 };
         int[] arrTwo = { 1, 2, 3, 4, 5 };
 
         boolean status = Arrays.equals(arrOne, arrTwo);
 
         if (status == true) {
+            System.out.println("Arrays are equals");
+        } else {
+            System.out.println("Arrays aren't equals");
+        }
+
+        // approach 2
+        boolean newStatus = true;
+
+        if (arrOne.length == arrTwo.length) {
+
+            for (int i = 0; i < arrOne.length; i++) {
+                if (arrOne[i] != arrTwo[i]) {
+                    newStatus = false;
+                }
+            }
+        } else {
+            newStatus = false;
+        }
+
+        if (newStatus == true) {
             System.out.println("Arrays are equals");
         } else {
             System.out.println("Arrays aren't equals");
