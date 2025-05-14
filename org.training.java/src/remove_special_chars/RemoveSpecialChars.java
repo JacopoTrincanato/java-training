aebe15663d549426940e6cbc893c9cf31d7f53ea
JacopoTrincanato <trincanatoj@gmail.com>
2025-05-14 17:22:15 +0200

    implement a way for remove all spaces in a string

--- org.training.java/src/remove_special_chars/RemoveSpecialChars.java
+++ org.training.java/src/remove_special_chars/RemoveSpecialChars.java
@@ -3,8 +3,12 @@ package remove_special_chars;
 public class RemoveSpecialChars {
     public static void main(String[] args) {
 
-        String specialChars = "!@#$%^&*()-_=+\\|{};:/?.>< Hello World";
+        String specialChars = "!@#$%^&*()-_=+\\|{};:/?.>< Hello      World";
         specialChars = specialChars.replaceAll("[^a-zA-Z0-9]", "");
+        System.out.println(specialChars);
+
+        // way for remove only spaces
+        specialChars = specialChars.replaceAll("\\s", "");
         System.out.println(specialChars);
     }
 }
