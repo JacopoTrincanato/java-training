53cb4569352ed42a880bf10b0ca5d918289e7749
JacopoTrincanato <trincanatoj@gmail.com>
2025-05-14 15:25:03 +0200

    implement an algorithm to check what are the duplicate values

--- org.training.java/src/duplicate_values/DuplicateValues.java
+++ org.training.java/src/duplicate_values/DuplicateValues.java
@@ -6,6 +6,22 @@ public class DuplicateValues {
         int[] clearArr = new int[numbArr.length];
         int j = 0;
 
+        // find duplicate values
+        boolean flag = false;
+        for (int i = 0; i < numbArr.length; i++) {
+            for (int l = i + 1; l < numbArr.length; l++) {
+                if (numbArr[i] == numbArr[l]) {
+                    System.out.println("the duplicate value is: " + numbArr[i]);
+                    flag = true;
+                }
+            }
+        }
+
+        if (flag == false) {
+            System.out.println("Duplicates not found");
+        }
+
+        // remove duplicate values
         for (int i = 0; i < numbArr.length - 1; i++) {
             if (numbArr[i] != numbArr[i + 1]) {
                 clearArr[j++] = numbArr[i];
