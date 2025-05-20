d90a89f1d83b2c7dec49a85c01a2126e9a3b43ae
JacopoTrincanato <trincanatoj@gmail.com>
2025-05-20 18:04:34 +0200

    implement an algorithm for print a sorted square pattern

--- org.training.java/src/print_patterns/PrintPatterns.java
+++ org.training.java/src/print_patterns/PrintPatterns.java
@@ -18,6 +18,15 @@ public class PrintPatterns {
             System.out.println();
         }
 
+        // sorted square pattern
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number; j++) {
+                System.out.print(i);
+            }
+
+            System.out.println();
+        }
+
         scanner.close();
     }
 }
