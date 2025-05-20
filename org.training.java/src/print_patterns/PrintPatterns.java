e3c790056717e0b5d79c091b75ee5b9c852bb0a5
JacopoTrincanato <trincanatoj@gmail.com>
2025-05-20 17:59:41 +0200

    implement an algorithm for print a square numbers pattern

---
+++ org.training.java/src/print_patterns/PrintPatterns.java
@@ -0,0 +1,23 @@
+package print_patterns;
+
+import java.util.Scanner;
+
+public class PrintPatterns {
+    public static void main(String[] args) {
+
+        System.out.println("Digit a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        // square pattern
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number; j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
